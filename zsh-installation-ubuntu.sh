d0075133caafe2222fc9a11e25f6b01955cdb1c4
DA <deepakd.coding@gmail.com>
2022-01-15 18:07:18 +0530

    apt install steps

---
+++ zsh-installation-ubuntu.sh
@@ -0,0 +1,9 @@
+sudo apt update && sudo apt install -y zsh
+chsh -s $(which zsh)
+sudo apt install -y powerline fonts-powerline
+sudo apt install -y zsh-theme-powerlevel9k && echo "source /usr/share/powerlevel9k/powerlevel9k.zsh-theme" >> ~/.zshrc
+sudo apt install -y zsh-syntax-highlighting && echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+
+
+
+#source: https://linuxhint.com/install_zsh_shell_ubuntu_1804/
